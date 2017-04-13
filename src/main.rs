b758715030b7f78d61c97f4c39e9252016243a22
Tiago Martinho <tiago.martinho@xpeppers.com>
2017-04-13 19:44:52 +0100

    Loop through guesses and exit when player wins

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,24 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less      => println!("Too small!"),
-        Ordering::Greater   => println!("Too big!"),
-        Ordering::Equal     => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal     => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
