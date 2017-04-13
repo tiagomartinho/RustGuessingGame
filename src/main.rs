9b221977b5e6c5be0c2f1bd8e17551680ff5f78f
Tiago Martinho <tiago.martinho@xpeppers.com>
2017-04-13 19:54:43 +0100

    Handle case when user does not input a numeric value

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,10 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less      => println!("Too small!"),
