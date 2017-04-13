1d8a9165f6e2c1e8effb9b65f15572fe586313b9
Tiago Martinho <tiago.martinho@xpeppers.com>
2017-04-13 19:56:31 +0100

    Remove debug print of secret_number

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
+
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101); 
-
-    println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
